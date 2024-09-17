9060392219ad3b1668f544708c6bbbe6de8c9ac3
Surendhar739 <surendhar.d@mrcooper.com>
2024-09-17 13:21:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,7 @@ from snowflake.snowpark.functions import col
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list=st.multiselect(
-    'choose upto 5 indredients:'
+ingredients_list=st.multiselect('choose upto 5 indredients:'
     , my_dataframe
     ,max_selections=5
 )
