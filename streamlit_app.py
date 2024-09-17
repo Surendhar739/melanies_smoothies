c73aa034921e63dd7d670b1ea6767e1195df6d47
Surendhar739 <surendhar.d@mrcooper.com>
2024-09-17 12:29:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session= cnx.session()
 
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
